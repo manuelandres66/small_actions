f1df8b8c32fe767ddf32761725b24dd77ab6eb71
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2021-01-20 11:24:14 -0500

    Babel production

--- static/maps/javascript/reactPoints.js
+++ static/maps/javascript/reactPoints.js
@@ -1,69 +1,103 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-const Result = (props) => {
-    return (
-        <a href={props.url} className="result"><div>
-            <h4>{props.name}</h4>
-            <h6>{props.organization}</h6>
-        </div></a>
-    )
-};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-class App extends React.Component {
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            results : []
-        };
-    };
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-    search = (query) => {
-        
-        if (query.target.value != ""){
-            fetch('/api/search_helps', {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers :{
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken' : document.querySelector('[name=csrfmiddlewaretoken]').value
-                },
-                body: JSON.stringify({'search' : query.target.value})
-            }).then(response => response.json())
-            .then(data => {
-                if (data.error != undefined) {
-                    console.error('Error in the petition')
-                } else {
-                    this.setState({
-                        results: data.results
-                    });
-                    // console.log(data);
-                    console.log(this.state);
-                };
-            })
-        };
-    };
-
-    render() {
-        return (
-            <div>
-                <div id="searchBar">
-                    <input type="text" id="search" onChange={this.search} autoComplete="off" placeholder="Buscar Lugares"/>
-                    <div id="search_container">
-                        <i className="fas fa-search" id="search"></i>
-                    </div>
-                </div>
-                <div id="results">
-                    {this.state.results.map(result => {
-                        return <Result name={result.name} organization={result.organization} url={result.url} key={result.name}/>
-                    })}
-                </div>
-            </div>
+var Result = function Result(props) {
+    return React.createElement(
+        "a",
+        { href: props.url, className: "result" },
+        React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "h4",
+                null,
+                props.name
+            ),
+            React.createElement(
+                "h6",
+                null,
+                props.organization
+            )
         )
-    }
+    );
 };
 
+var App = function (_React$Component) {
+    _inherits(App, _React$Component);
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('react')
-);
+    function App(props) {
+        _classCallCheck(this, App);
+
+        var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
+
+        _this.search = function (query) {
+
+            if (query.target.value != "") {
+                fetch('/api/search_helps', {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+                    },
+                    body: JSON.stringify({ 'search': query.target.value })
+                }).then(function (response) {
+                    return response.json();
+                }).then(function (data) {
+                    if (data.error != undefined) {
+                        console.error('Error in the petition');
+                    } else {
+                        _this.setState({
+                            results: data.results
+                        });
+                        // console.log(data);
+                        console.log(_this.state);
+                    };
+                });
+            };
+        };
+
+        _this.state = {
+            results: []
+        };
+        return _this;
+    }
+
+    _createClass(App, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                null,
+                React.createElement(
+                    "div",
+                    { id: "searchBar" },
+                    React.createElement("input", { type: "text", id: "search", onChange: this.search, autoComplete: "off", placeholder: "Buscar Lugares" }),
+                    React.createElement(
+                        "div",
+                        { id: "search_container" },
+                        React.createElement("i", { className: "fas fa-search", id: "search" })
+                    )
+                ),
+                React.createElement(
+                    "div",
+                    { id: "results" },
+                    this.state.results.map(function (result) {
+                        return React.createElement(Result, { name: result.name, organization: result.organization, url: result.url, key: result.name });
+                    })
+                )
+            );
+        }
+    }]);
+
+    return App;
+}(React.Component);
+
+;
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('react'));
