c9bd5c5f9b72e4d103eaf4961d3c90fb67bf6293
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2021-01-26 09:10:47 -0500

    Create Comment Model

--- maps/admin.py
+++ maps/admin.py
@@ -3,8 +3,9 @@ from . import models
 # Register your models here.
 
 class HelpAdmin(admin.ModelAdmin):
-    filter_horizontal = ("photos",)
+    filter_horizontal = ("photos", "comments")
 
 admin.site.register(models.Help, HelpAdmin)
 admin.site.register(models.Organization)
 admin.site.register(models.HelpPhoto)
+admin.site.register(models.Comment)
