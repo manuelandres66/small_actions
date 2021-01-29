a16433f41489288af6f4621c6adbad43b11177a6
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2021-01-29 16:07:54 -0500

    Test_org

--- info/tests.py
+++ info/tests.py
@@ -4,6 +4,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 from .models import Organization
 from .forms import NewOrganization
+from maps.models import Help
 
 import json
 # Create your tests here.
@@ -23,7 +24,7 @@ class OrgTest(TestCase):
 
     def setUp(self):
         uploaded = SimpleUploadedFile('test.gif', small_gif, content_type='image/gif')
-        Organization.objects.create(name="Manuels Organization", 
+        new_org = Organization.objects.create(name="Manuels Organization", 
                 short_description="Lorem ipsum dolor sit amet consectetur adipiscing elit placerat",
                 quote="Hello My Friends",
                 circular_icon = uploaded,
@@ -35,27 +36,48 @@ class OrgTest(TestCase):
                 circular_icon = uploaded,
                 image = uploaded)
 
-    def test_org(self):
+        self.help = Help.objects.create(name="Cole", latitude=1.215, longitude=-77.276, short_description="Lorem ipsum dolor sit amet consectetur adipiscing.", 
+        recomedations="Lorem ipsum dolor sit amet consectetur adipiscing..", organization=new_org, category="Cole", temporal_code='AAD-458-JJU')
+
+    def test_orgs(self):
         c = Client()
         response = c.get(reverse('searchOrganization'))
         self.assertEqual(response.status_code, 200)
 
-    def test_apiorg(self):
+    def test_apiorgs(self):
         c = Client()
         response = c.post(reverse('apiSearchOrg'), {'search' : 'M'}, content_type="application/json")
         data = json.loads(response.content)
+
         self.assertEqual(data['results'], [
             {
                 'name' : 'Manuels Organization',
-                'number_points': 0,
-                'url' : '/'
+                'number_points': 1,
+                'url' :  reverse('org', kwargs={'pk' : 1})
             }, 
             {
                 'name' : 'Manuels test Organization',
                 'number_points': 0,
-                'url' : '/'
+                'url' :  reverse('org', kwargs={'pk' : 2})
             }
         ])
+
+    def test_org(self):
+        c = Client()
+        response = c.get(reverse('org', kwargs={'pk' : 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_apiorg(self):
+        c = Client()
+        response = c.post(reverse('apiHelpOrg'), {'id' : 1}, content_type="application/json")
+        data = json.loads(response.content)
+
+        self.assertEqual(data['points'], [{
+            'name' : 'Cole',
+            'cordinates' : ['-77.27600000','1.21500000'],
+            'rute' : reverse('go', kwargs={'uuid' : self.help.uuid}),
+            'uuid' : reverse('info', kwargs={'uuid' : self.help.uuid})
+        }])
 
 
 
