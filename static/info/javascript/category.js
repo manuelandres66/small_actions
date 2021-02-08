ae7e08e033ce5daeb912b03b5594f40a68b422e4
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2021-02-08 09:45:07 -0500

    Dark Mode Correction

--- static/info/javascript/category.js
+++ static/info/javascript/category.js
@@ -9,7 +9,11 @@ const set_category_check = (category, set) => { //Function to Check Category
     if (set) {
         false_check.style.backgroundColor = false_check.getAttribute('data-color');
     } else {
-        false_check.style.backgroundColor = '#fff';
+        if (document.querySelector('html[theme="dark-mode"]') != undefined) {
+            false_check.style.backgroundColor = '#000';
+        } else {
+            false_check.style.backgroundColor = '#fff';
+        }
     }
 
 }
