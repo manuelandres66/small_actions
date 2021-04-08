1b44fda0d461facd8b20209dbfa57749a8adc4bf
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2021-04-08 11:09:38 -0500

    ads.txt

--- small_actions/urls.py
+++ small_actions/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('maps.urls')),
     path('account/', include('login.urls')),
     path('info/', include('info.urls')),
-    path('control/', include('control.urls'))
+    path('control/', include('control.urls')),
+    path("ads.txt", TemplateView.as_view(template_name="emails/ads.txt", content_type="text/plain")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
